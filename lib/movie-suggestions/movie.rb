6e344c824eaecff628651efe0ab99e8c0929f8b0
Allen Lu <allenlu95@yahoo.com>
2015-10-10 12:24:51 -0700

    Create points

--- lib/movie-suggestions/movie.rb
+++ lib/movie-suggestions/movie.rb
@@ -1,7 +1,7 @@
 class Movie
-  attr_reader :name, :year, :actors, :genre, :director
+  attr_reader :name, :year, :actors, :genres, :director
   
-  def intialize(name, year, actors, genre, director)
+  def intialize(name, year, actors, genres, director)
 
   end
 
