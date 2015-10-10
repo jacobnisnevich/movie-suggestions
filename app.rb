cd0c24094fb4eb6657fb5639057e32162b3eab6e
Allen Lu <allenlu95@yahoo.com>
2015-10-10 10:49:43 -0700

    Create post

--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ require File.expand_path('../lib/movie-suggestions.rb', __FILE__)
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
+
+post '/getMovieByCriteria' do
+
+end
+
+post '/getMovieByMovie' do
+
+end
