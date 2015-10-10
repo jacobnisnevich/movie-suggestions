0ea0ecc95abb6f7b3205e1db2da3f10187fc9e0e
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-10-10 09:48:44 -0700

    initial commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'json'
+
+require File.expand_path('../lib/movie-suggestions.rb', __FILE__)
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
---
+++ lib/movie-suggestions.rb
@@ -0,0 +1,8 @@
+require 'json'
+require 'spotlite'
+
+[
+  
+].each do |file_name|
+  require File.expand_path("../movie-suggestions/#{file_name}", __FILE__)
+end
